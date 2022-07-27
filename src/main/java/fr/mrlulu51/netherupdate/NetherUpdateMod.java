f814a699a6d0e324817abfa70a7427a0abd385e2
Mr-lulu-51 <lulubinou51@gmail.com>
2022-07-27 13:35:38 +0200

    Initial commit

---
+++ src/main/java/fr/mrlulu51/netherupdate/NetherUpdateMod.java
@@ -0,0 +1,10 @@
+package fr.mrlulu51.netherupdate;
+
+import fr.mrlulu51.netherupdate.util.Constants;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Constants.MODID, name = Constants.NAME, version = Constants.VERSION, acceptedMinecraftVersions = Constants.MC_VERSION)
+public class NetherUpdateMod {
+
+
+}
---
+++ src/main/java/fr/mrlulu51/netherupdate/util/Constants.java
@@ -0,0 +1,10 @@
+package fr.mrlulu51.netherupdate.util;
+
+public class Constants {
+
+    public static final String MODID = "netherupdate";
+    public static final String NAME = "Nether Update Mod";
+    public static final String VERSION = "1.0.0";
+    public static final String MC_VERSION = "1.12.2";
+    public static final String AUTHOR = "mrlulu51.jar";
+}
